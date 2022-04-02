d54874b418f3f014488dfc687db4eec673dff99e
WayneFerdon <wayneferdon@hotmail.com>
2022-04-02 11:16:15 +0800

    Create main.user.js

---
+++ main.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name            B站字幕字体自定义
+// @namespace    http://tampermonkey.net/
+// @version          1.0
+// @description    Customize Danmu in bilibili.com
+// @author           WayneFerdon
+// @match           https://www.bilibili.com/*
+// @require          http://code.jquery.com/jquery-1.11.0.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setInterval(function() {
+        setTimeout(function() {
+            changeDanmuFont();
+        }, 10);
+    }, 500);
+
+    function changeDanmuFont() {
+        function changeStyle(className, level=0) {
+            try
+            {
+                let danmu = $(document.getElementsByClassName(className)).children('div');
+                danmu.css('fontFamily','PingFang SF');
+                danmu.css('text-shadow','rgb(0, 0, 0) 1.5px 0px 1.5px, rgb(0, 0, 0) 0px 1.5px 1.5px, rgb(0, 0, 0) 0px -1.5px 1.5px, rgb(0, 0, 0) -1.5px 0px 1.5px');
+            }
+            catch(err)
+            {
+                if (level < 3) {
+                    setTimeout(function() {
+                        changeStyle(className, level+1);
+                    }, 100);
+                }
+            }
+        }
+        changeStyle('bilibili-player-video-danmaku');
+        changeStyle('bpx-player-row-dm-wrap');
+        return;
+
+    };
+})();
